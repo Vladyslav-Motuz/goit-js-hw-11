9c1e65c5f5342b6c43e04eb5f93117e0addf62e3
Vladyslav <vlad.motuz@gmail.com>
2021-07-14 14:08:05 +0300

    add color-switcher

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+// import '../sass/main.scss';
+
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let Interval = '';
+
+const onClickStart = () => {
+    if (Interval) {
+        return
+    }
+    
+    Interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);    
+}
+
+const onClickStop = () => {
+    clearInterval(Interval);
+    Interval = '';
+}
+
+start.addEventListener('click', onClickStart);
+stop.addEventListener('click', onClickStop);
---
+++ src/js/02-timer.js
@@ -0,0 +1,30 @@
+import '../sass/main.scss';
+
+const date = new Date();
+console.log(date);
+const btnStart = document.querySelector('[data-start]');
+console.log(btnStart);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+const dataStart = () => { };
+
+btnStart.addEventListener('click', dataStart);
